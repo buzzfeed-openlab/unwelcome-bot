3125edfa85de160d793848602e4097b8fd1e8a8a
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2015-11-14 18:17:34 -0800

    reallyActuallyTweet?

--- frisco-bot.js
+++ frisco-bot.js
@@ -17,7 +17,7 @@ var FriscoBot = module.exports = function(config) {
         var text = this.normalizeText(tweet.text);
 
         // filter out tweets that could be sensitive
-        var filteredWord = this.containsAnyOf(filterWords, text);
+        var filteredWord = containsAnyOf(filterWords, text);
         if (filteredWord) {
             console.log('filtered > ', filteredWord);
             return;
@@ -25,16 +25,27 @@ var FriscoBot = module.exports = function(config) {
 
         // if there are required words, filter tweets without them
         if (requiredWords && requiredWords.length) {
-            if (!this.containsAnyOf(requiredWords, text)) {
+            if (!containsAnyOf(requiredWords, text)) {
                 console.log('required > ', tweet.text);
                 return;
             }
         }
 
+        var response = this.generateResponse(tweet, config.responses);
+
         console.log('-------');
         console.log('user:', tweet.user.screen_name);
         console.log('tweet:', tweet.text);
+        console.log('response:', response);
         console.log('-------');
+
+        if (config.reallyActuallyTweet) {
+            twit.post('statuses/update', { status: response }, function(err, data, response) {
+                if (err) {
+                    console.log('Error:\n', err);
+                }
+            });
+        }
 
     }.bind(this));
 
@@ -47,11 +58,26 @@ FriscoBot.prototype.normalizeText = function(text) {
     return text.toLowerCase();
 };
 
-FriscoBot.prototype.containsAnyOf = function(needles, haystack) {
+FriscoBot.prototype.generateResponse = function(tweet, responses) {
+    var response = '@' + tweet.user.screen_name + ' ';
+    response += randFromArray(responses);
+
+    return response;
+};
+
+function containsAnyOf(needles, haystack) {
     for (var i = 0; i < needles.length; ++i) {
         if (haystack.indexOf(needles[i]) != -1) {
             return needles[i];
         }
     }
     return undefined;
+};
+
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+function randFromArray(arr) {
+    return arr[randInt(0, arr.length)];
 };
